840c3636205913ded436a2811c8d7c973a813000
Arun Sundaram <26311830+asundr@users.noreply.github.com>
2021-11-13 01:00:51 +1100

    Cleaned up Player class

--- Battleship/Player.cpp
+++ Battleship/Player.cpp
@@ -1,20 +1,31 @@
-#include "conio.h"
 #include "Player.h"
+#include "conio.h"
+#include "ControllerAI.h"
+#include "Grid.h"
 
 CPlayer::CPlayer(CGrid& _grid) : CController(_grid)
 {
 	m_grid.SetVisible(true);
+	m_selector = new Point{ 0, 0 };
+	m_selectorBounds = new Point{ 0, 0 };
 }
 
 CPlayer::~CPlayer()
 {
+	delete m_selector;
+	delete m_selectorBounds;
+}
 
+void CPlayer::Reset()
+{
+	CController::Reset();
+	SetSelector(0, 0);
+	SetSelectorBounds(0, 0);
 }
 
 short CPlayer::Turn(CController& _opponent)
 {
-	//CGrid& oGrid = _opponent.Grid();
-	SetSelectorBounds(_opponent.Grid(), m_selector.x, m_selector.y, 1, 1);
+	UpdateSelectorBounds(m_selector->x, m_selector->y, 1, 1, _opponent.Grid());
 	short type;
 	do
 	{
@@ -23,63 +34,52 @@ short CPlayer::Turn(CController& _opponent)
 	return type;
 }
 
-void CPlayer::Reset()
-{
-	CController::Reset();
-	m_selector = { 0, 0 };
-	m_selectorBounds = { 0, 0 };
-}
-
-void CPlayer::SetSelectorBounds(CGrid& _grid, short _x, short _y, short _width, short _height)
+void CPlayer::UpdateSelectorBounds(short _x, short _y, short _width, short _height, CGrid& _grid)
 {
 	RevertTiles(_grid);
-	m_selector = { _x, _y };
-	m_selectorBounds = { _width, _height };
+	SetSelector(_x, _y);
+	SetSelectorBounds(_width, _height);
 	DrawSelection(_grid);
 }
 
-void CPlayer::SetSelectorBounds(short _x, short _y, short _width, short _height)
+void CPlayer::UpdateSelectorBounds(short _x, short _y, short _width, short _height)
 {
-	SetSelectorBounds(m_grid, _x, _y, _width, _height);
+	UpdateSelectorBounds(_x, _y, _width, _height, m_grid);
 }
 
-bool CPlayer::SetSelector(Point& _coord, CGrid& _grid)
+bool CPlayer::UpdateSelector(const Point& _coord, CGrid& _grid)
 {
-	if (!_grid.IsRegionInBounds(_coord.x, _coord.y, m_selectorBounds.x, m_selectorBounds.y))
+	if (!_grid.IsRegionInBounds(_coord.x, _coord.y, m_selectorBounds->x, m_selectorBounds->y))
 	{
 		return false;
 	}
-	RevertTiles(_grid); //SetSelecotrBounds(_grid, ...)
-	m_selector = _coord;
-	DrawSelection(_grid);
+	UpdateSelectorBounds(_coord.x, _coord.y, m_selectorBounds->x, m_selectorBounds->y, _grid);
 	return true;
 }
 
-bool CPlayer::SetSelector(Point& _coord)
+bool CPlayer::UpdateSelector(const Point& _coord)
 {
-	return SetSelector(_coord, m_grid);
+	return UpdateSelector(_coord, m_grid);
 }
 
-bool CPlayer::ShiftSelector(Point& _shift, CGrid& _grid)
+bool CPlayer::ShiftSelector(const Point& _shift, CGrid& _grid)
 {
-	Point pNew = { m_selector.x + _shift.x , m_selector.y + _shift.y };
-	return SetSelector(pNew, _grid);
+	Point pNew = { m_selector->x + _shift.x , m_selector->y + _shift.y };
+	return UpdateSelector(pNew, _grid);
 }
 
-bool CPlayer::ShiftSelector(Point& _shift)
+bool CPlayer::ShiftSelector(const Point& _shift)
 {
 	return ShiftSelector(_shift, m_grid);
 }
 
 bool CPlayer::ToggleSelectorRotation(CGrid& _grid)
 {
-	if (!_grid.IsRegionInBounds(m_selector.x, m_selector.y, m_selectorBounds.y, m_selectorBounds.x))
+	if (!_grid.IsRegionInBounds(m_selector->x, m_selector->y, m_selectorBounds->y, m_selectorBounds->x))
 	{
 		return false;
 	}
-	RevertTiles(_grid);
-	m_selectorBounds = { m_selectorBounds.y, m_selectorBounds.x };
-	DrawSelection(_grid);
+	UpdateSelectorBounds(m_selector->x, m_selector->y, m_selectorBounds->y, m_selectorBounds->x);
 	return true;
 }
 
@@ -88,70 +88,72 @@ bool CPlayer::ToggleSelectorRotation()
 	return ToggleSelectorRotation(m_grid);
 }
 
-void CPlayer::RevertTiles(CGrid& _grid)
+void CPlayer::RevertTiles(CGrid& _grid) const
 {
-	_grid.RevertTiles(m_selector.x, m_selector.y, m_selectorBounds.x, m_selectorBounds.y);
+	_grid.RevertTiles(m_selector->x, m_selector->y, m_selectorBounds->x, m_selectorBounds->y);
 }
 
 void CPlayer::DrawSelection(CGrid& _grid) const
 {
-	if (m_selectorBounds.x == m_selectorBounds.y) // TODO add better check for ship placement vs firing
+	if (m_selectorBounds->x == m_selectorBounds->y) // TODO add better check for ship placement vs firing
 	{
-		if (_grid.GetTile(m_selector.x, m_selector.y).CanHit())
+		if (_grid.GetTile(m_selector->x, m_selector->y).CanHit())
 		{
-			_grid.DrawSelection(m_selector.x, m_selector.y, m_selectorBounds.x, m_selectorBounds.y);
+			_grid.DrawSelection(m_selector->x, m_selector->y, m_selectorBounds->x, m_selectorBounds->y);
 		}
 		else
 		{
-			_grid.DrawSelectionError(m_selector.x, m_selector.y, m_selectorBounds.x, m_selectorBounds.y);
+			_grid.DrawSelectionError(m_selector->x, m_selector->y, m_selectorBounds->x, m_selectorBounds->y);
 		}
 	}
-	else if (_grid.IsRegionEmpty(m_selector.x, m_selector.y, m_selectorBounds.x, m_selectorBounds.y))
+	// placing ships 
+	else if (_grid.IsRegionEmpty(m_selector->x, m_selector->y, m_selectorBounds->x, m_selectorBounds->y))
 	{
-		_grid.DrawSelection(m_selector.x, m_selector.y, m_selectorBounds.x, m_selectorBounds.y);
+		_grid.DrawSelection(m_selector->x, m_selector->y, m_selectorBounds->x, m_selectorBounds->y);
 	}
 	else
 	{
-		_grid.DrawSelectionError(m_selector.x, m_selector.y, m_selectorBounds.x, m_selectorBounds.y);
+		_grid.DrawSelectionError(m_selector->x, m_selector->y, m_selectorBounds->x, m_selectorBounds->y);
 	}
 }
 
 
 void CPlayer::PlaceShips()
 {
-	for (short i = m_shipCount - 1; i >= 0; --i)
+	for (short i = s_shipTypeCount - 1; i >= 0; --i)
 	{
-		if (m_selectorBounds.x < m_selectorBounds.y)
+		if (m_selectorBounds->x < m_selectorBounds->y)  // if selector is vertical
 		{
-			SetSelectorBounds(m_selector.x, m_selector.y, 1, m_ships[i]);
+			UpdateSelectorBounds(m_selector->x, m_selector->y, 1, m_ships[i]);
 		}
-		else
+		else    // if selector is horizontal
 		{
-			SetSelectorBounds(m_selector.x, m_selector.y, m_ships[i], 1);
+			UpdateSelectorBounds(m_selector->x, m_selector->y, m_ships[i], 1);
 		}
-		while (! HandleSelctionInput(i + 2));
+		while (! HandleSelctionInput(IndexToType(i)) );
 	}
-	SetSelectorBounds(0, 0, 0, 0);
+	RevertTiles(m_grid);
+	UpdateSelectorBounds(0, 0, 0, 0);
 }
 
 short CPlayer::HandleSelctionInput(short _value, CController& _controller)
 {	
 	CGrid& grid = _controller.Grid();
 	int input = _getch();
-	if (input == 101)
+	if (input == 101) // E
 	{
 		ToggleSelectorRotation(grid);
 	}
-	else if (input == 32)
+	else if (input == 32) // SPACE
 	{
 		if (_value == 0)
 		{
-			if (grid.CanHitTile(m_selector.x, m_selector.y))
+			if (grid.CanHitTile(m_selector->x, m_selector->y))
 			{
-				return grid.HitTile(m_selector.x, m_selector.y);
+				return grid.HitTile(m_selector->x, m_selector->y);
 			}
 		}
-		else if (grid.TryToPlaceShip(m_selector.x, m_selector.y, m_selectorBounds.x, m_selectorBounds.y, _value, false))
+		else if (grid.TryToPlaceShip(m_selector->x, m_selector->y, m_selectorBounds->x, m_selectorBounds->y, _value, false))
 		{
 			return _value;
 		}
@@ -159,7 +161,7 @@ short CPlayer::HandleSelctionInput(short _value, CController& _controller)
 	else
 	{
 		Point dx;
-		switch (input)
+		switch (input)	// WASD
 		{
 		case 97:
 			dx = { -1, 0 }; break;
@@ -180,4 +182,16 @@ short CPlayer::HandleSelctionInput(short _value, CController& _controller)
 short CPlayer::HandleSelctionInput(short _value)
 {
 	return HandleSelctionInput(_value, *this);
+}
+
+void CPlayer::SetSelector(short _x, short _y)
+{
+	m_selector->x = _x;
+	m_selector->y = _y;
+}
+
+void CPlayer::SetSelectorBounds(short _x, short _y)
+{
+	m_selectorBounds->x = _x;
+	m_selectorBounds->y = _y;
 }
--- Battleship/Player.h
+++ Battleship/Player.h
@@ -1,30 +1,34 @@
 #pragma once
 
 #include "Controller.h"
-#include "ControllerAI.h"
+
+struct Point;
+class CControllerAI;
 
 class CPlayer : public CController
 {
 public:
 	CPlayer(CGrid& _grid);
 	~CPlayer();
-	short Turn(CController& _opponent);
 	void Reset();
-	void SetSelectorBounds(CGrid& _grid, short _x, short _y, short _width, short _height);
-	void SetSelectorBounds(short _x, short _y, short _width, short _height);
-	bool SetSelector(Point& _coord, CGrid& _grid);
-	bool SetSelector(Point& _coord);
-	bool ShiftSelector(Point& _shift, CGrid& _grid);
-	bool ShiftSelector(Point& _shift);
+	short Turn(CController& _opponent);
+	void UpdateSelectorBounds(short _x, short _y, short _width, short _height, CGrid& _grid);
+	void UpdateSelectorBounds(short _x, short _y, short _width, short _height);
+	bool UpdateSelector(const Point& _coord, CGrid& _grid);
+	bool UpdateSelector(const Point& _coord);
+	bool ShiftSelector(const Point& _shift, CGrid& _grid);
+	bool ShiftSelector(const Point& _shift);
 	bool ToggleSelectorRotation(CGrid& _grid);
 	bool ToggleSelectorRotation();
-	void RevertTiles(CGrid& _grid);
+	void RevertTiles(CGrid& _grid) const;
 	void DrawSelection(CGrid& _grid) const;
 	void PlaceShips();
 	short HandleSelctionInput(short _value);
 	short HandleSelctionInput(short _value, CController& _opponent);
-private:
-	Point m_selector = { 0, 0 };
-	Point m_selectorBounds = { 0, 0 };
+protected:
+	Point* m_selector;
+	Point* m_selectorBounds;
+	void SetSelector(short _x, short _y);
+	void SetSelectorBounds(short _x, short _y);
 };
 
